b67f3b995da3ce2e7a04994ba4016153f0c9c86b
Mike Fisk <mfisk@lanl.gov>
2002-11-16 23:22:56 -0700

    major rewrite to handle more cases (for groupby)

--- libsmacq/sched-iter.c
+++ libsmacq/sched-iter.c
@@ -8,7 +8,14 @@ struct runq {
 };
 
 void runable(struct runq ** runqp, struct filter * f, const dts_object * d) {
-  struct runq * entry = malloc(sizeof(struct runq));
+  struct runq * entry;
+
+  if (f && (f->status & SMACQ_END)) return;
+
+  //fprintf(stderr, "runable %p type %d\n", d, dts_gettype(d));
+  assert(!d || dts_gettype(d));
+
+  entry = malloc(sizeof(struct runq));
   entry->f = f;
   entry->d = d;
   entry->next = *runqp;
@@ -50,51 +57,79 @@ void do_shutdown(struct runq ** runqp, struct filter *);
 void check_for_shutdown(struct runq ** runqp, struct filter *f) {
   int i;
   for (i=0; i < f->numchildren; i++) {
-    if (f->next[i]) return;
+    if (f->next[i] && (! (f->next[i]->status & SMACQ_END))) 
+      return;
   }
 
   /* No more active children.  Clean-up self and parents. */
-  do_shutdown(runqp, f);
+  for (i=0; i < f->numchildren; i++) {
+    if (f->next[i] && (! (f->next[i]->status & SMACQ_END))) 
+      flow_sched_iterative_shutdown(runqp, f->next[i]);
+  }
+  //do_shutdown(runqp, f);
 }
 
 void do_shutdown(struct runq ** runqp, struct filter *f) {
-  int i, j;
+  int i;
 
   if (f->shutdown) {
     f->shutdown(f->state);
   }
 
+  //fprintf(stderr, "module %p %s ended\n", f, f->name);
+  f->status = SMACQ_END;
+
   /* Propagate to children */
   for (i=0; i < f->numchildren; i++) {
     /* Enqueue a terminate record */
-    runable(runqp, f->next[i], NULL);
-
-    for (j=0; j < f->next[i]->numparents; j++) {
-      if (f->next[i]->parent[j] == f) {
-	f->next[i]->parent[j] = NULL;
-      }
+    if (! (f->next[i]->status & SMACQ_END)) {
+      flow_sched_iterative_shutdown(f->next[i], (void**)runqp);
     }
-
-    f->next[i] = NULL;
   }
 
   /* Propagate to parents */
   for (i=0; i< f->numparents; i++) {
-    for (j=0; j < f->parent[i]->numchildren; j++) {
-      if (f->parent[i]->next[j] == f) {
-	f->parent[i]-> next[j] = NULL;
-	check_for_shutdown(runqp, f->parent[i]);
+    check_for_shutdown(runqp, f->parent[i]);
+  }
+}
+
+static int flow_sched_iterative_graph_alive (struct filter *f) {
+  int i;
+
+  if (! (f->status & SMACQ_END)) {
+    return 1;
+  }
+
+  for (i=0; i<f->numchildren; i++) {
+    if (f->next[i]) {
+      if (flow_sched_iterative_graph_alive(f->next[i])) {
+	return 1;
       }
     }
   }
 
-  smacq_free_module(f);
+  return 0;
 }
-    
+
 void flow_sched_iterative_shutdown(struct filter * startf, void ** state) {
   struct runq ** runqp = (struct runq **)state;
-  runable(runqp, startf, NULL);
+
+  struct runq * entry = malloc(sizeof(struct runq));
+  entry->f = startf;
+  entry->d = NULL;
+  entry->next = NULL;
+
+  // Append at end of queue
+  if (! *runqp) {
+    *runqp = entry;
+  } else {
+    struct runq * next;
+
+    for (next=*runqp; next->next; next=next->next);
+    next->next = entry;
+  }
 }
+
 
 /*
  * If dout is non-NULL, when an object is passed or produced by a leaf node, this function will return it in dout.
@@ -129,7 +164,7 @@ int flow_sched_iterative(struct filter * startf, const dts_object * din, const d
       
       if (retval & SMACQ_END) {
 	/* Enqueue a terminate record */
-	runable(runqp, startf, NULL);
+	flow_sched_iterative_shutdown(startf, (void**)runqp);
       }
     }
     
@@ -142,20 +177,10 @@ int flow_sched_iterative(struct filter * startf, const dts_object * din, const d
       int outchan = -1;
       int pretval = 0;
 
-      pop_runable(runqp);
-      
-      if (!d) {
-	/* Must terminate.  Not illegal to call scheduler again. */
-	if (f) do_shutdown(runqp, f);
-	//(*runqp)->f = NULL;
-	*dout = NULL;
-	return SMACQ_END;
-      }
-
-      //fprintf(stderr, "Pulled %p off queue for module %p\n", d, f);
-      
       if (!f) {
 	/* Datum fell off end of data-flow graph */
+	pop_runable(runqp);
+
 	if (dout) {
 	  *dout = d;
 	  return SMACQ_PASS;
@@ -165,26 +190,50 @@ int flow_sched_iterative(struct filter * startf, const dts_object * din, const d
 	}
       }
       
-      retval = f->consume(f->state, d, &outchan);
+      if (!d) {
+	if (f->status & SMACQ_END) {
+	  pop_runable(runqp);
       
+	  do_shutdown(runqp, f);
+	  //(*runqp)->f = NULL;
+	  *dout = NULL;
+	  if (*runqp || flow_sched_iterative_graph_alive(startf)) {
+	    return SMACQ_FREE;
+	  } else {
+	    assert(!*runqp);
+	    return SMACQ_END;
+	  } 
+	} else {
+	  /* Last call */
+	  /* We don't pop_runable so that we'll come back again and do_shutdown after last call */
+	  f->status = SMACQ_END;
+	  retval = SMACQ_PRODUCE;
+	}
+      } else {
+	pop_runable(runqp);
+
+	retval = f->consume(f->state, d, &outchan);
+
+	if (retval & SMACQ_PASS) {
+	  //fprintf(stderr, "Pass on %p by %p\n", d, f);
+	  queue_children(runqp, f, d, outchan);
+	}
+
+	dts_decref(d);
+      }
+
       if (retval & (SMACQ_PRODUCE|SMACQ_CANPRODUCE)) {
 	int outchan = -1;
-	const dts_object * d;
+	const dts_object * d = NULL;
 	pretval = f->produce(f->state, &d, &outchan);
 	
-	queue_children(runqp, f, d, outchan);
-      }
-      
-      if (retval & SMACQ_PASS) {
-	//fprintf(stderr, "Pass on %p by %p\n", d, f);
 	queue_children(runqp, f, d, outchan);
       }
       
       if ((retval|pretval) & SMACQ_END) {
 	do_shutdown(runqp, f);
       }
-      
-      dts_decref(d);
+
     }
   }
 
