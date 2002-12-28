aded1737e404e1bf3436585febadcb8ba7eed974
Mike Fisk <mfisk@lanl.gov>
2002-12-28 10:56:24 -0700

    removed -s option

--- bin/smacqp.c
+++ bin/smacqp.c
@@ -8,8 +8,6 @@
 #include <string.h>
 #include <smacq-internal.h>
 
-struct filter * parse_stmt(int, char **);
-
 struct thread_args {
   struct filter * f;
   struct smacq_init * context;
@@ -19,13 +17,8 @@ int main(int argc, char ** argv) {
   struct filter * objs;
 
   assert(argc > 1);
-/*
-  if (strcmp(argv[1], "-s") == 0) {
-    objs = parse_stmt(argc-1, argv+1);
-  }
-  else*/ {
-    objs = smacq_build_pipeline(argc-1, argv+1);
-  }
+  objs = smacq_build_pipeline(argc-1, argv+1);
+
   return smacq_start(objs, RECURSIVE, NULL);
 }
 
