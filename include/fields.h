f4e86b0b1d3f65cf805d69aad2a520cbc601f570
Mike Fisk <mfisk@lanl.gov>
2003-01-21 18:48:19 -0700

    oops! removed a function that is used (once)

--- include/fields.h
+++ include/fields.h
@@ -15,6 +15,15 @@ struct fieldset {
   int lasttype;
 };
 
+
+static inline int smacq_nextfielddata(struct fieldset * fieldset, const dts_object **d, int i) {
+     if (i >= fieldset->num) 
+	     	return 0;
+
+     *d = fieldset->currentdata[i];
+     return (i+1);
+}
+
 extern struct iovec * fields2vec(smacq_environment * env, const dts_object * datum, struct fieldset * fieldset);
 void fields_init(smacq_environment * env, struct fieldset * fieldset, int argc, char ** argv);
 
