89503e1434c2ea5cc623dc06b1af0dc641329b2c
Mike Fisk <mfisk@lanl.gov>
2003-02-01 16:27:12 -0700

    added outputq routines for modules to use.

--- include/types-inline.c
+++ include/types-inline.c
@@ -423,5 +423,51 @@ static inline const dts_object * smacq_construct_fromstring(smacq_environment * 
   }
 }
 
+struct smacq_outputq {
+	const dts_object * o;
+	int outchan;
+	struct smacq_outputq * next;
+	struct smacq_outputq * end;
+};
+
+static inline void smacq_produce_enqueue(struct smacq_outputq ** qp, const dts_object * o, int outchan) {
+  struct smacq_outputq * nq = malloc(sizeof(struct smacq_outputq));
+  assert(qp);
+
+  nq->o = o;
+  nq->outchan = outchan;
+  nq->next = NULL;
+
+  if (!*qp) {
+	  *qp = nq;
+  } else {
+	  (*qp)->end->next = nq;
+  }
+
+  (*qp)->end = nq;
+}
+
+static inline smacq_result smacq_produce_dequeue(struct smacq_outputq ** qp, const dts_object ** o, int * outchan) {
+  struct smacq_outputq * head = *qp;
+
+  if (!head) {
+	  return SMACQ_FREE;
+  }
+
+  *o = head->o;
+  *outchan = head->outchan;
+
+  *qp = head->next;
+  (*qp)->end = head->end;
+
+  free(head);
+
+  if (*qp) {
+	  return SMACQ_PASS|SMACQ_PRODUCE;
+  } else {
+	  return SMACQ_PASS;
+  }
+}
+
 #endif
 
