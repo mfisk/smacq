18b3f37631b09f0f7661068552e82ab72cfdbbc0
Mike Fisk <mfisk@lanl.gov>
2003-02-01 12:49:57 -0700

    Return raw data now

--- types/bytes.c
+++ types/bytes.c
@@ -4,12 +4,8 @@
 #include "smacq.h"
 
 static int smacqtype_bytes_get_string(const dts_object * o, dts_object * field) {
-  char buf[64]; // Only has to hold log10(2**32)
-
-  snprintf(buf, 64, "data at %p", dts_getdata(o));
-  field->data= strdup(buf);
-  field->len= strlen(buf);
-  field->free_data = 1;
+  field->data = o->data;
+  field->len = o->len;
 
   return 1;
 }
