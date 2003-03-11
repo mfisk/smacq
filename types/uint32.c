7995fd6e8afd7208d6b0e17de70df6ccb909ee3a
Mike Fisk <mfisk@lanl.gov>
2003-03-11 14:59:25 -0700

    leading 0x smaller buffer

--- types/uint32.c
+++ types/uint32.c
@@ -10,9 +10,9 @@ static int smacqtype_uint32_get_string(const dts_object * o, dts_object * field)
 }
 
 static int smacqtype_uint32_get_hexstring(const dts_object * o, dts_object * field) {
-	  dts_setsize(field, 64); // Only has to hold log10(2**32)
-	    snprintf(field->data, 64, "%x", dts_data_as(o, unsigned int));
-	      return 1;
+  dts_setsize(field, 16); // 4 bytes -> 8 chars + 3 = 11
+  snprintf(field->data, 64, "0x%x", dts_data_as(o, unsigned int));
+  return 1;
 }
 
 static int smacqtype_uint32_get_double(const dts_object * o, dts_object * field) {
