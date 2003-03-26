81cd75c60bfad1870c27a0440f203908f869fdc9
Mike Fisk <mfisk@lanl.gov>
2003-03-26 08:16:39 -0700

    moved to misc

--- bin/reloc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-RUNDIR=`echo $0 | sed 's/\/[^/]*$//'`
-CMD=`basename $0`
-LD_LIBRARY_PATH=${RUNDIR}/.lib:${LD_LIBRARY_PATH} exec -a "$0" ${RUNDIR}/.bin/${CMD} "$@"
-#LD_LIBRARY_PATH=${RUNDIR}/.lib:${LD_LIBRARY_PATH} ${RUNDIR}/.bin/${CMD} "$@"
