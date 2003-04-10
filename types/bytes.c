84eec61fb49bf4e3e4cecf61563b153fd24b57a7
Mike Fisk <mfisk@lanl.gov>
2003-04-10 15:10:48 -0600

    added URL decoding field

--- types/bytes.c
+++ types/bytes.c
@@ -13,8 +13,41 @@ static int smacqtype_bytes_get_string(const dts_object * o, dts_object * field) 
   return 1;
 }
 
+static int smacqtype_bytes_url_decode(const dts_object * o, dts_object * field) {
+  unsigned char * p, * dp, * end;
+  int shrink = 0;
+  char hex[3];
+
+  hex[2] = '\0';
+  dts_setsize(field, o->len);
+  p = dts_getdata(o);
+  end = p + dts_getsize(o);
+  dp = dts_getdata(field);
+
+  while (p < end) {
+	  if (*p == '%' && (p+2 < end)) {
+		shrink += 2;
+		memcpy(hex, p+1, 2);
+		if (0 < sscanf(hex, "%2hhx", dp)) {
+			p += 3;
+			dp++;
+		} else {
+	  		*dp++ = *p++;
+		}
+	  } else {
+	  	*dp++ = *p++;
+	  }
+  }
+
+  dts_setsize(field, o->len - shrink);
+
+  return 1;
+}
+
+
 struct dts_field_spec dts_type_bytes_fields[] = {
   { "string",   "string",	smacqtype_bytes_get_string },
+  { "bytes",    "urldecode",	smacqtype_bytes_url_decode },
   { END,        NULL }
 };
 
