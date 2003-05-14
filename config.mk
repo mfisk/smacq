8cd484c0321aaf6b4a932c4d4ee6eda09b133d9b
Mike Fisk <mfisk@lanl.gov>
2003-05-14 13:55:17 -0600

    make sure that include/glib-2.0 comes before include/

--- config.mk
+++ config.mk
@@ -5,5 +5,5 @@ CFLAGS=-O9 -Winline 			# Optimized for normal use
 CFLAGS=-ggdb -O0 -fno-inline -Winline	# For debugging
 CFLAGS=-O0 -fno-inline -Winline		# MacOS/X doesn't like -ggdb 
 
-CFLAGS+=$(COPTS)
 CFLAGS+=-I$(USR)/include/glib-2.0 -I$(USR)/lib/glib-2.0/include -I$(TOPSRCDIR)/include -I$(SRCDIR)
+CFLAGS+=$(COPTS)
