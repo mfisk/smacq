7b473d941d29046cfcebe40f21472f64585d85af
Mike Fisk <mfisk@lanl.gov>
2003-05-26 18:00:02 -0600

    -Winline is common to all flagsets

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 #
 # This file is included by all Makefiles.
 #
-CFLAGS=-O9 -Winline 			# Optimized for normal use
-CFLAGS=-ggdb -O0 -fno-inline -Winline	# For debugging
-CFLAGS=-O0 -fno-inline -Winline		# MacOS/X doesn't like -ggdb 
+CFLAGS=-O9  			# Optimized for normal use
+CFLAGS=-ggdb -O0 -fno-inline 	# For debugging
+CFLAGS=-O0 -fno-inline 		# MacOS/X doesn't like -ggdb 
 
-CFLAGS+=-I$(USR)/include/glib-2.0 -I$(USR)/lib/glib-2.0/include -I$(TOPSRCDIR)/include -I$(SRCDIR)
+CFLAGS+=-Winline -I$(USR)/include/glib-2.0 -I$(USR)/lib/glib-2.0/include -I$(TOPSRCDIR)/include -I$(SRCDIR)
 CFLAGS+=$(COPTS)
