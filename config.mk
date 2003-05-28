159a92718d87df73d4ac0b29abb862bcdb676c1c
Mike Fisk <mfisk@lanl.gov>
2003-05-27 19:06:54 -0600

    optimize by default

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 #
 # This file is included by all Makefiles.
 #
-CFLAGS=-O9  			# Optimized for normal use
+CFLAGS=-g -O0 -fno-inline 	# MacOS/X doesn't like -ggdb 
 CFLAGS=-ggdb -O0 -fno-inline 	# For debugging
-CFLAGS=-O0 -fno-inline 		# MacOS/X doesn't like -ggdb 
+CFLAGS=-O9  			# Optimized for normal use
 
 CFLAGS+=-Winline -I$(USR)/include/glib-2.0 -I$(USR)/lib/glib-2.0/include -I$(TOPSRCDIR)/include -I$(SRCDIR)
 CFLAGS+=$(COPTS)
