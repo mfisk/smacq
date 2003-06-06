83a9d0874b0f435e079a411f1007df6cb9466e7e
Mike Fisk <mfisk@lanl.gov>
2003-06-06 14:09:37 -0600

    mmap returns -1 on error (not NULL)

--- modules/pcapfile.c
+++ modules/pcapfile.c
@@ -251,6 +251,7 @@ static inline int open_filename(struct state * state, char * filename) {
     fstat(fileno(state->fh), &stats);
 
     state->mmap = mmap(NULL, stats.st_size, PROT_READ, MAP_SHARED, fileno(state->fh), 0);
+    if ((int)state->mmap == -1) state->mmap = NULL;
     
     if (state->mmap) {
       state->mmap_end = state->mmap + stats.st_size;
