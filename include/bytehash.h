e875f6d547fcdea43e60312f0787767bfa24d491
Mike Fisk <mfisk@lanl.gov>
2003-08-31 09:56:04 -0600

    GHashTableofBytes now struct iovec_hash

--- include/bytehash.h
+++ include/bytehash.h
@@ -1,28 +1,22 @@
 #include <glib.h>
 #include <sys/uio.h>
 
-struct bytedata {
-  int len;
-  unsigned char * bytes;
-  struct GHashTableofBytes * b;
-  int expired;
-};
-
-typedef struct GHashTableofBytes GHashTableofBytes;
+struct element;
 enum chaining_boolean { CHAIN, NOCHAIN };
 enum free_boolean { FREE, NOFREE };
 
-GHashTableofBytes * bytes_hash_table_new(int maxkeybytes, enum chaining_boolean, enum free_boolean);
-int bytes_hash_table_incrementv(GHashTableofBytes * ht, struct iovec *, int count);
-int bytes_hash_table_insertv(GHashTableofBytes * ht, struct iovec *, int count,  gpointer value);
-gpointer bytes_hash_table_replacev(GHashTableofBytes * ht, struct iovec * keys, int count, gpointer value);
-gpointer bytes_hash_table_lookupv(GHashTableofBytes * ht, struct iovec *, int);
-int bytes_hash_table_removev(GHashTableofBytes * ht, struct iovec *, int);
-void bytes_hash_table_destroy(GHashTableofBytes * ht);
-gint bytes_hash_table_lookup_extendedv(GHashTableofBytes * ht, struct iovec * key, int keys, gpointer *oldkey, gpointer *current);
+struct iovec_hash * bytes_hash_table_new(int maxkeybytes, enum chaining_boolean, enum free_boolean);
+int bytes_hash_table_incrementv(struct iovec_hash * ht, struct iovec *, int count);
+int bytes_hash_table_insertv(struct iovec_hash * ht, struct iovec *, int count,  gpointer value);
+gpointer bytes_hash_table_replacev(struct iovec_hash * ht, struct iovec * keys, int count, gpointer value);
+gpointer bytes_hash_table_lookupv(struct iovec_hash * ht, struct iovec *, int);
+int bytes_hash_table_removev(struct iovec_hash * ht, struct iovec *, int);
+void bytes_hash_table_destroy(struct iovec_hash * ht);
+gint bytes_hash_table_lookup_extendedv(struct iovec_hash * ht, struct iovec * key, int keys, gpointer *oldkey, gpointer *current);
 void bytes_init_hash(guint32** randoms, int num, unsigned long prime);
-// gint bytes_hash_table_lookup_extended(GHashTableofBytes * ht, int keysize, guint16 * key, gpointer current);
+// gint bytes_hash_table_lookup_extended(struct iovec_hash * ht, int keysize, guint16 * key, gpointer current);
 
-guint32 bytes_hash_valuev(GHashTableofBytes * ht, int nvecs, struct iovec *);
-void bytes_hash_table_foreach(GHashTableofBytes * ht, GHFunc func, gpointer user_data);
-void bytes_hash_table_foreach_remove(GHashTableofBytes * ht, GHRFunc func, gpointer user_data);
+guint32 bytes_hash_valuev(struct iovec_hash * ht, int nvecs, struct iovec *);
+void bytes_hash_table_foreach(struct iovec_hash * ht, GHFunc func, gpointer user_data);
+void bytes_hash_table_foreach_remove(struct iovec_hash * ht, GHRFunc func, gpointer user_data);
+int bytes_mask(const struct element * b1, struct iovec * iovecs, int nvecs);
