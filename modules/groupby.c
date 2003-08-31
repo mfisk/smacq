b9d7501bb5101c53c8895ddc4a8da430afd20f5f
Mike Fisk <mfisk@lanl.gov>
2003-08-31 17:20:47 -0600

    less code, more queueing, much clearer

--- modules/groupby.c
+++ modules/groupby.c
@@ -28,14 +28,12 @@ struct state {
 
   struct output * cont;
 
-  int lastcall;
   struct iovec * partitionv;
 }; 
 
 struct output {
   smacq_graph * graph;
   struct runq * runq;
-  int shutdown:1;
 };
 
 /* Bucket may no longer be valid after this call */
@@ -44,7 +42,7 @@ static inline int run_and_queue(struct state * state, struct output* partition) 
     do {
 	const dts_object * product = NULL;
     	more = smacq_sched_iterative_busy(partition->graph, &product, partition->runq, 0);
-	//fprintf(stderr, "run_and_queue produced %p with status %x\n", product, more);
+	fprintf(stderr, "run_and_queue produced %p with status %x\n", product, more);
 	if (product) smacq_produce_enqueue(&state->outputq, product, -1);
     	if (more & SMACQ_END) {
       		assert(!partition->runq);
@@ -55,6 +53,22 @@ static inline int run_and_queue(struct state * state, struct output* partition) 
     } while (more);
     return more;
 }
+
+static inline void destroy_partition(struct state * state, struct output * partition) {
+  fprintf(stderr, "doing shutdown of %p\n", partition->graph);
+  smacq_sched_iterative_shutdown(partition->graph, partition->runq);
+  run_and_queue(state, partition);
+}
+
+static int destroy_partition_wrapper(gpointer key, gpointer value, gpointer userdata) {
+  struct state * state = (struct state *)userdata;
+  struct output * partition = (struct output*)value;
+
+  destroy_partition(state, partition);
+
+  return 1;
+}
+
 
 /* Bucket may no longer be valid after this call */
 static inline int run(struct state * state, struct output* partition) {
@@ -91,7 +105,7 @@ static inline struct output * get_partition(struct state * state, struct iovec *
   if (!partition) {
     partition =  g_new0(struct output, 1);
     //fprintf(stderr, "New partition %p\n", partition);
-    partition->graph = smacq_build_pipeline(state->queryargc, state->queryargv);
+    partition->graph = smacq_build_query(state->env->types, state->queryargc, state->queryargv);
     smacq_start(partition->graph, ITERATIVE, state->env->types);
     smacq_sched_iterative_init(partition->graph, &partition->runq, 0);
     bytes_hash_table_insertv(state->hashtable, state->partitionv, state->fieldset.num, partition);
@@ -107,9 +121,7 @@ static int check_invalidate(gpointer key, gpointer value, gpointer userdata) {
 
   if (bytes_mask(element, state->partitionv, state->fieldset.num)) {
     //fprintf(stderr, "groupby got a partial refresh\n");
-    smacq_sched_iterative_shutdown(partition->graph, partition->runq);
-    partition->shutdown = 1;
-    run_and_queue(state, partition);
+    destroy_partition(state, partition);
     return 1;
   }
 
@@ -139,19 +151,15 @@ static smacq_result groupby_consume(struct state * state, const dts_object * dat
      partition = get_partition(state, state->partitionv);
 
      if (dts_gettype(datum) == state->refresh_type) {
+
        // Handle refresh (kill-off child)
-       
+       destroy_partition(state, partition);
        int res = bytes_hash_table_removev(state->hashtable, state->partitionv, state->fieldset.num);
        assert(res);
-       fprintf(stderr, "groupby remove from %p\n", state->hashtable);
-       smacq_sched_iterative_shutdown(partition->graph, partition->runq);
      } else {
        smacq_sched_iterative_input(partition->graph, datum, partition->runq);
+       run(state, partition);
      }
-
-     /* Run may invalidate the partition, so we shouldn't use that variable
-      * later in this function. */
-     run(state, partition);
   }
   
   // Producing input is just a PASS
@@ -214,25 +222,6 @@ static smacq_result groupby_init(struct smacq_init * context) {
   return 0;
 }
 
-static int destroy_partition(gpointer key, gpointer value, gpointer userdata) {
-  struct state * state = (struct state *)userdata;
-  struct output * partition = (struct output*)value;
-
-  if (!partition->shutdown) {
-	partition->shutdown = 1;
-  	//fprintf(stderr, "doing shutdown of %p\n", partition->graph);
-  	smacq_sched_iterative_shutdown(partition->graph, partition->runq);
-  }
-
-  if ((SMACQ_END & run(state, partition))) {
-  	//fprintf(stderr, "Last call for partition %p.  Product now %p\n", partition, state->product);
-	return 1;
-  } else {
-  	//fprintf(stderr, "Postponed last call for partition %p.  Product now %p\n", partition, state->product);
-	return 0;
-  }
-}
-
 static smacq_result groupby_produce(struct state * state, const dts_object ** datump, int * outchan) {
   int status = SMACQ_FREE;
   int lastcall = 1;
@@ -251,18 +240,16 @@ static smacq_result groupby_produce(struct state * state, const dts_object ** da
     lastcall = 0;
   }
 
-  if (state->lastcall || lastcall) {
+  if (lastcall) {
     /* This must be last-call, because we didn't ask for it */
     /* Notify all children */
-    bytes_hash_table_foreach_remove(state->hashtable, destroy_partition, state);
-    //fprintf(stderr, "finished a round of last call with %p, %p, %p\n", *datump, state->product, state->cont);
+    fprintf(stderr, "groupby got last call\n");
+    bytes_hash_table_foreach_remove(state->hashtable, destroy_partition_wrapper, state);
   }
 
-  if (!*datump && state->cont) {
-	return groupby_produce(state, datump, outchan);
-  }
+  if (state->cont) status |= SMACQ_PRODUCE;
 
-  return status | (state->cont || (SMACQ_PRODUCE & smacq_produce_canproduce(&state->outputq))); 
+  return (status | smacq_produce_canproduce(&state->outputq)); 
 }
 
 /* Right now this serves mainly for type checking at compile time: */
