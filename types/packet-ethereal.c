df68d19f51da46ec27fc5528873c624cde3cfde1
Mike Fisk <mfisk@lanl.gov>
2003-10-20 13:27:49 -0600

    only warn once

--- types/packet-ethereal.c
+++ types/packet-ethereal.c
@@ -1,6 +1,11 @@
 #include <smacq.h>
 
+static int Warned = 0;
+
 int epan_getfield(const dts_object * packet, dts_object * fieldo, dts_field_element element) {
-	fprintf(stderr, "packet: warning: EPAN fields not available in this build\n");
+	if (!Warned) {
+		fprintf(stderr, "packet: warning: EPAN fields not available in this build\n");
+		Warned = 1;
+	}
 	return 0;
 }
