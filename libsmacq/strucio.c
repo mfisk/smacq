865082995012947bf83e7d2c5bfe2ac4478db5d4
Mike Fisk <mfisk@lanl.gov>
2003-11-21 14:55:34 -0700

    new strucio routines

---
+++ libsmacq/strucio.c
@@ -0,0 +1,338 @@
+#ifdef linux
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE64_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <zlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <strucio.h>
+
+struct strucio {
+  char * oneshot_filename;
+
+  /* Call-backs */
+  strucio_newfile_fn * newfile_fn;
+  void * newfile_data;
+  strucio_nextfilename_fn * nextfilename_fn;
+  void * nextfilename_data;
+  enum strucio_read_type read_type;
+
+  /* Currenly opened file */
+  char * filename;
+  FILE * fh;
+
+  /* For zlib */
+  int use_gzip;
+  gzFile * gzfh;
+
+  /* For mmap */
+  void * mmap;
+  void * mmap_current;
+  void * mmap_end;
+
+  /* For rotation */
+  long long outputleft;
+  long long maxfilesize;
+  int suffix;
+
+  /* For filelist_args */
+  int argc;
+  char ** argv;
+};
+
+struct strucio * strucio_init() {
+  struct strucio * rdr = calloc(1, sizeof(struct strucio));
+  return rdr;
+}
+
+void strucio_set_use_gzip(struct strucio * r, int use_gzip) {
+  r->use_gzip = use_gzip;
+}
+
+void strucio_register_newfile(struct strucio * r, strucio_newfile_fn * fn, void * data) {
+  r->newfile_fn = fn;
+  r->newfile_data = data;
+}  
+
+void strucio_register_filelist(struct strucio * r, strucio_nextfilename_fn * fn, void * data) {
+  r->nextfilename_fn = fn;
+  r->nextfilename_data = data;
+  r->oneshot_filename = NULL;
+}
+
+void strucio_register_file(struct strucio * r, char * filename) {
+  r->nextfilename_fn = NULL;
+  r->oneshot_filename = filename;
+}
+
+void strucio_set_read_type(struct strucio * r, enum strucio_read_type read_type) {
+  r->read_type = read_type;
+}
+
+void strucio_set_rotate(struct strucio * r, long long size) {
+  r->maxfilesize = size;
+}
+
+static char * filelist_stdin(struct strucio * r, void * data) {
+  int res;
+  char * filename = NULL;
+
+  res = scanf("%4096s", filename);
+
+  if (res == EOF) {
+    return NULL;
+  } else if (res != 1) {
+    fprintf(stderr, "Error reading next filename from STDIN\n");
+    return NULL;
+  } else {
+    return filename;
+  }
+}
+
+static char * filelist_args(struct strucio * rdr, void * data) {
+  if (! rdr->argc) {
+    //fprintf(stderr, "No more files to read from\n");
+    return(NULL);
+  } else {
+    char * filename = rdr->argv[0];
+    rdr->argc--; rdr->argv++;
+    return filename;
+  }
+}
+
+void strucio_register_filelist_stdin(struct strucio * r) {
+  strucio_register_filelist(r, filelist_stdin, NULL);
+}
+
+void strucio_register_filelist_args(struct strucio * r, int argc, char ** argv) {
+  r->argc = argc;
+  r->argv = argv;
+  strucio_register_filelist(r, filelist_args, NULL);
+}
+
+
+static inline void * read_mmap(struct strucio * rdr, int len) {
+      void * current = rdr->mmap_current;
+      rdr->mmap_current += len;
+
+      if (rdr->mmap_current > rdr->mmap_end) {
+	if ((rdr->mmap_current - len) > rdr->mmap_end) {
+	  fprintf(stderr, "structured read: premature end of mmapped file\n");
+	}
+	return NULL;
+      }
+
+      return current;
+}
+
+static inline void * read_current_file(struct strucio * rdr, void * buf, int len) {
+      int retval;
+
+      if (rdr->mmap) {
+	void * map = read_mmap(rdr, len);
+	if (!map) return map;
+	memcpy(buf, map, len);
+	return buf;
+      } else if (rdr->gzfh) {
+	retval = gzread(rdr->gzfh, buf, len);
+	//fprintf(stderr, "gzread returned %d\n", retval);
+	return( (retval==1) ? buf : NULL );
+      } else if (rdr->fh) {
+	retval = fread(buf, len, 1, rdr->fh);
+	//fprintf(stderr, "fread returned %d on length %u\n", retval, len);
+	return( (retval==1) ? buf : NULL );
+      } else {
+	/* Need to open a file */
+	return(NULL);
+      }
+}
+
+
+static inline void close_file(struct strucio* rdr) {
+  if (rdr->gzfh) {
+    gzclose(rdr->gzfh);
+    rdr->gzfh = NULL;
+  }
+  
+  if (rdr->fh) {
+    fclose(rdr->fh);
+    rdr->fh = NULL;
+  }
+
+  if (rdr->mmap) {
+    rdr->mmap = NULL;
+  }
+}
+
+/* Read from this file or the next one */
+/* Return 1 on success, 0 on failure */
+static inline void * strucio_read_multi(struct strucio * rdr, void * buf, int len, enum strucio_read_type read_type) {
+  void * result;
+
+  if (read_type == MMAP && !rdr->mmap) {
+    /* Can't fulfil MMAP request */
+    return NULL;
+  }
+    
+  while (1) {
+    if (rdr->mmap && read_type != COPY) {
+      result = read_mmap(rdr, len);
+    } else {
+      result = read_current_file(rdr, buf, len);
+    }
+
+    if (result) return result;
+      
+    /* Need to try another file */
+    close_file(rdr);
+    if (!strucio_open(rdr)) {
+      return NULL;
+    } 
+  }
+}
+
+void * strucio_read_copy(struct strucio * rdr, void * buf, int len) {
+  return strucio_read_multi(rdr, buf, len, COPY);
+}
+
+void * strucio_read_mmap(struct strucio * rdr, int len) {
+  return strucio_read_multi(rdr, NULL, len, MMAP);
+}
+
+void * strucio_read(struct strucio * rdr, void * buf, int len) {
+  return strucio_read_multi(rdr, buf, len, EITHER);
+}
+
+static inline int open_filename(struct strucio * rdr, char * filename) {
+  int try_mmap = 0;
+
+  if ((!filename) || (!strcmp(filename, "-"))) {
+    if (rdr->use_gzip) {
+	    rdr->gzfh = gzdopen(0, "rb");
+    } else {
+	    rdr->fh = fdopen(0, "r");
+    }
+  } else {
+    if (rdr->use_gzip) {
+	    rdr->gzfh = gzopen(filename, "rb");
+    } else {
+	    rdr->fh = fopen(filename, "r");
+	    try_mmap = (rdr->read_type != COPY);
+    }
+  }
+
+  if (!rdr->fh && !rdr->gzfh) {
+    perror("structured open");
+    exit(-1);
+  }
+
+  if (try_mmap) {
+    struct stat stats;
+    fstat(fileno(rdr->fh), &stats);
+
+    rdr->mmap = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE|MAP_NORESERVE, fileno(rdr->fh), 0);
+    if ((int)rdr->mmap == -1) rdr->mmap = NULL;
+    
+    if (rdr->mmap) {
+      rdr->mmap_end = rdr->mmap + stats.st_size;
+      rdr->mmap_current = rdr->mmap + ftell(rdr->fh);
+    } else {
+      fprintf(stderr, "structured: Error memory mapping file.  Will use stream I/O.\n");
+    }
+  }
+  return(1); /* success */
+}
+
+int strucio_open(struct strucio * rdr) {
+  char * filename;
+
+  if (rdr->oneshot_filename) {
+    filename = rdr->oneshot_filename;
+    rdr->oneshot_filename = NULL;
+  } else if (rdr->nextfilename_fn) {
+    filename = rdr->nextfilename_fn(rdr, rdr->nextfilename_data);
+  } else {
+    return -1;
+  }
+
+  if (!filename) return 0;
+
+  if (0 == open_filename(rdr, filename)) return 0;
+
+  if (rdr->newfile_fn) {
+    return rdr->newfile_fn(rdr, rdr->newfile_data);
+  }
+  
+  return 0;
+}
+
+
+/* -1 iff error */
+static int strucio_openwrite(struct strucio * rdr) {
+  char sufbuf[256];
+  char * filename;
+  
+  if (! rdr->fh) {
+    rdr->filename = rdr->oneshot_filename;
+    fprintf(stderr, "Output will be placed in structured file %s\n", rdr->filename);
+    
+    if (rdr->maxfilesize) {
+      snprintf(sufbuf, 256, "%s.%02d", rdr->filename, rdr->suffix);
+      filename = sufbuf;
+      rdr->suffix++;
+      
+      rdr->outputleft = rdr->maxfilesize;
+    } else {
+      filename = rdr->filename;
+    }
+    
+    if (!strcmp(filename, "-")) {
+      rdr->fh = stdout;
+    } else {
+      rdr->fh = fopen(filename, "w");
+    }
+    assert(rdr->fh);
+
+    if (rdr->newfile_fn) {
+      return rdr->newfile_fn(rdr, rdr->newfile_data);
+    }
+  
+  }
+
+  return 0;
+}
+
+int strucio_write(struct strucio * rdr, void * record, int len) {
+  assert(record);
+
+  rdr->outputleft -= len;
+
+  //fprintf(stderr, "%lld left\n", rdr->outputleft);
+  if (rdr->maxfilesize && (rdr->outputleft <= 0)) {
+    fclose(rdr->fh);
+    rdr->fh = NULL;
+  }
+
+  if (0 != strucio_openwrite(rdr)) 
+    return -1;
+  
+  if (1 != fwrite(record, len,  1, rdr->fh)) {
+    perror("structured write");
+    return -1;
+  }
+	
+  return 0;
+}
+
+void strucio_close(struct strucio * rdr) {
+  if (rdr->fh) { fclose(rdr->fh);}
+  if (rdr->gzfh) { gzclose(rdr->gzfh); }
+  free(rdr);
+}
--- modules/pcapfile.c
+++ modules/pcapfile.c
@@ -1,8 +1,3 @@
-#ifdef linux
-#define _FILE_OFFSET_BITS 64
-#define _LARGEFILE64_SOURCE
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -11,14 +6,9 @@
 #include <smacq.h>
 #include <dts_packet.h>
 #include <pcap.h>
-#include <zlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <sys/mman.h>
-
-enum file_read_type { EITHER, COPY, MAP };
-static int open_file(struct state * state);
-static inline void * read_current_file(struct state * state, void * buf, int len, enum file_read_type read_type);
+#include <strucio.h>
 
 static struct smacq_options options[] = {
   {"w", {string_t:"-"}, "Output file", SMACQ_OPT_TYPE_STRING},
@@ -35,28 +25,15 @@ struct state {
   int argc;
   char ** argv;
   int dts_pkthdr_type;		
+  struct strucio * rdr;
+  const dts_object * current_datum;
 
   /* Booleans */
   int produce;			/* Does this instance produce */
-  int gzip;
   int swapped;
   int extended;
   int hdr_size;
-  int avoid_mmap;
-
-  void * mmap;
-  void * mmap_current;
-  void * mmap_end;
-
-  gzFile * gzfh;
-  FILE * fh;
   struct pcap_file_header pcap_file_header;
-
-  long long outputleft;
-  unsigned long long maxfilesize;
-  int suffix;
-  int file_list;
-  char * filename;
 
   const dts_object * snaplen_o, * linktype_o;
   dts_field snaplen_field, linktype_field;
@@ -149,157 +126,28 @@ static inline void fixup_pcap(struct state * state, struct old_pcap_pkthdr * hdr
   }
 }
 
-static inline void * read_current_file(struct state * state, void * buf, int len, enum file_read_type read_type) {
-    if (state->mmap) {
-      void * current = state->mmap_current;
-      state->mmap_current += len;
+int newfile_read(struct strucio * rdr, void * data) {
+  struct state * state = data;
 
-      if (state->mmap_current > state->mmap_end) {
-	if ((state->mmap_current - len) > state->mmap_end) {
-	  fprintf(stderr, "pcapfile: premature end of mmapped file\n");
-	}
-	return NULL;
-      }
-      
-      if (read_type == COPY) {
-	memcpy(buf, current, len);
-	return buf;
-      } else {
-	return current;
-      }
-    } else {
-      int retval;
-      assert(read_type != MAP);
-      
-      if (state->gzfh) {
-	retval = gzread(state->gzfh, buf, len);
-	//fprintf(stderr, "gzread returned %d\n", retval);
-	return( (retval==1) ? buf : NULL );
-      } else if (state->fh) {
-	retval = fread(buf, len, 1, state->fh);
-	//fprintf(stderr, "fread returned %d on length %u\n", retval, len);
-	return( (retval==1) ? buf : NULL );
-      } else {
-	assert(!"pcapfile lost filehandle");
-	return(NULL);
-      }
-    }
-}
-
-
-static inline void close_file(struct state* state) {
-  if (state->gzfh) {
-    gzclose(state->gzfh);
-    state->gzfh = NULL;
-  }
-  
-  if (state->fh) {
-    fclose(state->fh);
-    state->fh = NULL;
-  }
-
-  if (state->mmap) {
-    state->mmap = NULL;
-  }
-}
-
-/* Read from this file or the next one */
-static inline void * read_file(struct state * state, void * buf, int len, enum file_read_type read_type) {
-  void * result;
-
-  while (1) {
-    result = read_current_file(state, buf, len, read_type);
-    if (result) return result;
-
-    close_file(state);
-    if (!open_file(state)) {
-      return NULL;
-    } 
-  }
-
-}
-
-
-static inline int open_filename(struct state * state, char * filename) {
-  int try_mmap = 0;
-
-  if ((!filename) || (!strcmp(filename, "-"))) {
-    if (state->gzip) {
-	    state->gzfh = gzdopen(0, "rb");
-    } else {
-	    state->fh = fdopen(0, "r");
-    }
-  } else {
-    if (state->gzip) {
-	    state->gzfh = gzopen(filename, "rb");
-    } else {
-	    state->fh = fopen(filename, "r");
-	    try_mmap = ! state->avoid_mmap;
-    }
-  }
-
-  if (!state->fh && !state->gzfh) {
-    perror("pcapfile open");
-    exit(-1);
-  }
-  if (!read_file(state, &state->pcap_file_header, sizeof(struct pcap_file_header), COPY)) {
+  if (!strucio_read_copy(rdr, &state->pcap_file_header, sizeof(struct pcap_file_header))) {
     perror("pcapfile read");
     exit(-1);
   }
   
   parse_pcapfile(state, &state->pcap_file_header);
-  
-  if (try_mmap && !state->swapped && !state->extended) {
-    struct stat stats;
-    fstat(fileno(state->fh), &stats);
 
-    state->mmap = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE|MAP_NORESERVE, fileno(state->fh), 0);
-    if ((int)state->mmap == -1) state->mmap = NULL;
-    
-    if (state->mmap) {
-      state->mmap_end = state->mmap + stats.st_size;
-      state->mmap_current = state->mmap + ftell(state->fh);
-    } else {
-      fprintf(stderr, "pcapfile: Error memory mapping file.  Will use stream I/O.\n");
-    }
-  }
-
-  fprintf(stderr, "pcapfile: Opening %s for read ( ", filename);
+  //  fprintf(stderr, "pcapfile: Opening %s for read ( ", state->rdr->filename);
+  fprintf(stderr, "pcapfile: Opening for read ( ");
 
   if (state->swapped) fprintf(stderr, "byte-swapped ");
   else fprintf(stderr, "host-byte-order ");
 
   if (state->extended) fprintf(stderr, "extended-header ");
-  if (state->mmap) fprintf(stderr, "memory-mapped ");
+  //  if (state->rdr->mmap) fprintf(stderr, "memory-mapped ");
 
   fprintf(stderr, ")\n");
 
-  return(1); /* success */
-}
-
-static int open_file(struct state * state) {
-  char * filename = "";
-  char next_file[4096];
-
-  if (state->file_list) {
-	  int res;
-	  filename = next_file;
-	  res = scanf("%4096s", filename);
-	  if (res == EOF) {
-		  return(0);
-	  } else if (res != 1) {
-		  fprintf(stderr, "Error reading next filename from STDIN\n");
-		  return(0);
-	  }
-  } else if (! state->argc) {
-    //fprintf(stderr, "No more files to read from\n");
-    return(0);
-  } else {
-    filename = state->argv[0];
-    state->argc--; state->argv++;
-  }
-
-  return open_filename(state, filename);
+  return 0;
 }
 
 static smacq_result pcapfile_produce(struct state * state, const dts_object ** datump, int * outchan) {
@@ -314,7 +162,7 @@ static smacq_result pcapfile_produce(struct state * state, const dts_object ** d
 
   pkt = (struct dts_pkthdr*)dts_getdata(datum);
 
-  hdrp = read_file(state, &pkt->pcap_pkthdr, state->hdr_size, EITHER);
+  hdrp = strucio_read(state->rdr, &pkt->pcap_pkthdr, state->hdr_size);
   if (!hdrp) return SMACQ_END;
 
   fixup_pcap(state, hdrp);
@@ -325,24 +173,23 @@ static smacq_result pcapfile_produce(struct state * state, const dts_object ** d
   dts_incref(state->snaplen_o, 1);
   dts_attach_field(datum, state->snaplen_field, state->snaplen_o);
 
+  if (state->extended) {
+    const dts_object * newo;
+    struct extended_pkthdr * ehdr = (struct extended_pkthdr*)(hdrp+1);
+    
+    newo = smacq_dts_construct(state->env, state->ifindex_type, &ehdr->ifindex);
+    dts_attach_field(datum, state->ifindex_field, newo);
+    
+    newo = smacq_dts_construct(state->env, state->protocol_type, &ehdr->protocol);
+    dts_attach_field(datum, state->protocol_field, newo);
+    
+    newo = smacq_dts_construct(state->env, state->pkt_type_type, &ehdr->pkt_type);
+    dts_attach_field(datum, state->pkt_type_field, newo);
+  }
+  
   if (hdrp == &pkt->pcap_pkthdr) {
-
-    if (state->extended) {
-      const dts_object * newo;
-      struct extended_pkthdr * ehdr = (struct extended_pkthdr*)(hdrp+1);
-
-      newo = smacq_dts_construct(state->env, state->ifindex_type, &ehdr->ifindex);
-      dts_attach_field(datum, state->ifindex_field, newo);
-      
-      newo = smacq_dts_construct(state->env, state->protocol_type, &ehdr->protocol);
-      dts_attach_field(datum, state->protocol_field, newo);
-      
-      newo = smacq_dts_construct(state->env, state->pkt_type_type, &ehdr->pkt_type);
-      dts_attach_field(datum, state->pkt_type_field, newo);
-    }
-
     //fprintf(stderr, "reading packet of caplen %d\n", hdrp->caplen);
-    if (!read_file(state, hdrp + 1, hdrp->caplen, COPY)) 
+    if (!strucio_read_copy(state->rdr, hdrp + 1, hdrp->caplen)) 
       return SMACQ_END;
 
   } else {
@@ -350,7 +197,8 @@ static smacq_result pcapfile_produce(struct state * state, const dts_object ** d
 
     ((dts_object *)datum)->data = hdrp;
     ((dts_object *)datum)->len = sizeof(struct dts_pkthdr) + hdrp->caplen;
-    payload = read_file(state, NULL, hdrp->caplen, MAP);
+
+    payload = strucio_read_mmap(state->rdr, hdrp->caplen);
 
     if (payload != hdrp +1) {
       //fprintf(stderr, "payload at %p instead of %p\n", payload, hdrp + 1);
@@ -364,115 +212,78 @@ static smacq_result pcapfile_produce(struct state * state, const dts_object ** d
   return SMACQ_PASS|SMACQ_PRODUCE;
 }
 
-/* -1 iff error */
-static int pcapfile_openwrite(struct state * state, const dts_object * datum) {
-  char sufbuf[256];
-  char * filename;
+int newfile_write(struct strucio * rdr, void * data) {
+  struct state * state = data;
   const dts_object * linktype_o, * snaplen_o;
   int linktype, snaplen;
-  
-  if (! state->fh) {
-    fprintf(stderr, "Output will be placed in pcapfile %s\n", state->filename);
 
-    if (state->maxfilesize) {
-      snprintf(sufbuf, 256, "%s.%02d", state->filename, state->suffix);
-      filename = sufbuf;
-      state->suffix++;
-      
-      state->outputleft = state->maxfilesize - sizeof(struct pcap_file_header);
-    } else {
-      filename = state->filename;
-      state->outputleft = 1;
-    }
-    
-    if (!strcmp(filename, "-")) {
-      state->fh = stdout;
-    } else {
-      state->fh = fopen(filename, "w");
-    }
-    assert(state->fh);
-  
-    linktype_o = smacq_getfield(state->env, datum, state->linktype_field, NULL);
-    if (linktype_o) {
-      linktype = dts_data_as(linktype_o, int);
-      dts_decref(linktype_o);
-    } else {
-      fprintf(stderr, "pcapfile: open: warning no linktype!\n");
-      linktype = 1;
-    }
-    
-    snaplen_o = smacq_getfield(state->env, datum, state->snaplen_field, NULL);
-    if (snaplen_o) {
-      snaplen = dts_data_as(snaplen_o, int);
-      dts_decref(snaplen_o);
-    } else {
-      fprintf(stderr, "pcapfile: open: warning no snaplen!\n");
-      snaplen = 1514;
-    }
-    
-    fprintf(stderr, "pcapfile: Info: Opening %s for output (linktype %d, snaplen %d)\n", 
-	    filename, linktype, snaplen);
-    
-    state->pcap_file_header.snaplen = snaplen;
-    state->pcap_file_header.linktype = linktype;
-    
-    if (1 != fwrite(&state->pcap_file_header, sizeof(state->pcap_file_header), 1, state->fh)) {
-      perror("pcapfile write");
-      return -1;
-    }
+  linktype_o = smacq_getfield(state->env, state->current_datum, state->linktype_field, NULL);
+  if (linktype_o) {
+    linktype = dts_data_as(linktype_o, int);
+    dts_decref(linktype_o);
+  } else {
+    fprintf(stderr, "pcapfile: open: warning no linktype!\n");
+    linktype = 1;
   }
+  
+  snaplen_o = smacq_getfield(state->env, state->current_datum, state->snaplen_field, NULL);
+  if (snaplen_o) {
+    snaplen = dts_data_as(snaplen_o, int);
+    dts_decref(snaplen_o);
+  } else {
+    fprintf(stderr, "pcapfile: open: warning no snaplen!\n");
+    snaplen = 1514;
+  }
+  
+  /*
+  fprintf(stderr, "pcapfile: Info: Opening %s for output (linktype %d, snaplen %d)\n", 
+	  filename, linktype, snaplen);
+  */
 
+  fprintf(stderr, "pcapfile: Info: Opening output (linktype %d, snaplen %d)\n", 
+	  linktype, snaplen);
+  
+  state->pcap_file_header.snaplen = snaplen;
+  state->pcap_file_header.linktype = linktype;
+
+  if (-1 == strucio_write(state->rdr, &state->pcap_file_header, sizeof(state->pcap_file_header))) {
+    perror("pcapfile write");
+    return -1;
+  }
+ 
   return 0;
 }
 
 static smacq_result pcapfile_consume(struct state * state, const dts_object * datum, int * outchan) {
   assert(datum);
 
+  state->current_datum = datum;
+
   if (dts_gettype(datum) == state->dts_pkthdr_type) {
-	struct dts_pkthdr * pkt = dts_getdata(datum);
+    struct dts_pkthdr * pkt = dts_getdata(datum);
 
-	state->outputleft -= (sizeof(struct pcap_pkthdr) + pkt->pcap_pkthdr.caplen);
-	//fprintf(stderr, "%lld left\n", state->outputleft);
-	if (state->maxfilesize && (state->outputleft <= 0)) {
-	  fclose(state->fh);
-	  state->fh = NULL;
-	}
-
-	if (-1 == pcapfile_openwrite(state, datum)) 
-	  return(SMACQ_PASS|SMACQ_END|SMACQ_ERROR);
-
-	if (0) {
-	  int tot = fwrite(&pkt->pcap_pkthdr, sizeof(pkt->pcap_pkthdr), 1, state->fh);
-	  //tot += fwrite(&pkt->extended, sizeof(pkt->extended), 1, state->fh);
-	  tot += fwrite(pkt+1, pkt->pcap_pkthdr.caplen, 1, state->fh);
-	  if (tot != 3) {
-		perror("pcapfile write");
-		return(SMACQ_PASS|SMACQ_END|SMACQ_ERROR);
-	  }
-	} else {
-	  if (1 != fwrite(&pkt->pcap_pkthdr, sizeof(pkt->pcap_pkthdr) + pkt->pcap_pkthdr.caplen, 1, state->fh)) {
-		perror("pcapfile write");
-		return(SMACQ_PASS|SMACQ_END|SMACQ_ERROR);
-	  }
-	};
+    if (-1 == strucio_write(state->rdr, pkt, sizeof(struct pcap_pkthdr) + pkt->pcap_pkthdr.caplen)) {
+      return(SMACQ_PASS|SMACQ_END|SMACQ_ERROR);
+    }
 
   } else {
-	fprintf(stderr, "Received unknown data type (expected packet)\n");
-	exit(-1);
+    fprintf(stderr, "Received unknown data type (expected packet)\n");
+    exit(-1);
   }
 	
   return SMACQ_PASS;
 }
 
 static smacq_result pcapfile_shutdown(struct state * state) {
-	close_file(state);
-	free(state);
-
-	return 0;
+  strucio_close(state->rdr);
+  free(state);
+  
+  return 0;
 }
 
 static smacq_result pcapfile_init(struct smacq_init * context) {
   struct state * state;
+  smacq_opt output, size, list, gzip, avoid_mmap;
 
   //fprintf(stderr, "Loading pcapfile (%d,%d)\n", context->isfirst, context->islast);
   smacq_requiretype(context->env, "packet");
@@ -480,10 +291,9 @@ static smacq_result pcapfile_init(struct smacq_init * context) {
   context->state = state = (struct state*) calloc(sizeof(struct state),1);
   assert(state);
 
+  state->rdr = strucio_init();
   state->env = context->env;
   {
-    smacq_opt output, size, list, gzip, avoid_mmap;
-
     struct smacq_optval optvals[] = {
       { "w", &output}, 
       { "s", &size}, 
@@ -497,14 +307,21 @@ static smacq_result pcapfile_init(struct smacq_init * context) {
 				 &state->argc, &state->argv,
 				 options, optvals);
     
-    state->filename = output.string_t;
-    state->file_list = list.boolean_t;
-    state->maxfilesize = size.uint32_t * 1024 * 1024;
-    state->outputleft = 1024*1024;
-    state->gzip = gzip.boolean_t;
-    state->avoid_mmap = avoid_mmap.boolean_t;
+    if (list.boolean_t) {
+      strucio_register_filelist_stdin(state->rdr);
+    } else {
+      strucio_register_filelist_args(state->rdr, state->argc, state->argv);
+    }
+
+    if (size.uint32_t) {
+      strucio_set_rotate(state->rdr, size.uint32_t * 1024 * 1024);
+    }
+    if (avoid_mmap.boolean_t) {
+      strucio_set_read_type(state->rdr, COPY);
+    }
+    strucio_set_use_gzip(state->rdr, gzip.boolean_t);
   }
-  
+
   state->snaplen_type = smacq_requiretype(state->env, "int");
   state->linktype_type = smacq_requiretype(state->env, "int");
 
@@ -512,6 +329,7 @@ static smacq_result pcapfile_init(struct smacq_init * context) {
   state->linktype_field = smacq_requirefield(state->env, "linktype");
 
   if (context->isfirst) {
+    strucio_register_newfile(state->rdr, newfile_read, state);
     //fprintf(stderr, "Reading pcapfile (no predecessor)\n");
     state->produce = 1;
 
@@ -522,9 +340,11 @@ static smacq_result pcapfile_init(struct smacq_init * context) {
     state->ifindex_field = smacq_requirefield(state->env, "ifindex");
     state->protocol_field = smacq_requirefield(state->env, "ethertype");
     state->pkt_type_field = smacq_requirefield(state->env, "pkt_type");
-
-    open_file(state);
+    assert(!strucio_open(state->rdr));
   } else if (context->islast) {
+    strucio_register_newfile(state->rdr, newfile_write, state);
+    strucio_register_file(state->rdr, output.string_t);
+
     state->pcap_file_header.magic = TCPDUMP_MAGIC;
     state->pcap_file_header.version_major = 2;
     state->pcap_file_header.version_minor = 4;
@@ -543,9 +363,9 @@ static smacq_result pcapfile_init(struct smacq_init * context) {
 
 /* Right now this serves mainly for type checking at compile time: */
 struct smacq_functions smacq_pcapfile_table = {
-  &pcapfile_produce, 
-  &pcapfile_consume,
-  &pcapfile_init,
-  &pcapfile_shutdown
+  produce: &pcapfile_produce, 
+  consume: &pcapfile_consume,
+  init: &pcapfile_init,
+  shutdown: &pcapfile_shutdown
 };
 
