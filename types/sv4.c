f2740f327761219174fe9928810b14e6c7af2eb0
Mike Fisk <mfisk@lanl.gov>
2003-12-01 16:45:17 -0700

    payload field works now

--- types/sv4.c
+++ types/sv4.c
@@ -1,27 +1,30 @@
 #include <smacq.h>
 
 static int get_sv4_data(const dts_object * datum, dts_object * data) {
-	return 0;
+	data->data = datum->data + 51;
+	data->len = datum->len - 51;
+	return (data->len > 0);
 }
 
 struct dts_field_spec dts_type_sv4_fields[] = {
 	{ "uint64",	"id",		NULL },
 	{ "ip",		"dstip",	NULL },
 	{ "ip",		"srcip",	NULL },
-	{ "nushort",	"dstport",	NULL },
-	{ "nushort",	"srcport",	NULL },
-	{ "nuint32",	"dstpackets",	NULL },
-	{ "nuint32",	"srcpackets",	NULL },
-	{ "nuint32",	"dstbytes",	NULL },
-	{ "nuint32",	"srcbytes",	NULL },
+	{ "ushort",	"dstport",	NULL },
+	{ "ushort",	"srcport",	NULL },
+	{ "uint32",	"dstpackets",	NULL },
+	{ "uint32",	"srcpackets",	NULL },
+	{ "uint32",	"dstbytes",	NULL },
+	{ "uint32",	"srcbytes",	NULL },
 
-	{ "ntime",	"start",	NULL },
-	{ "nushort",	"startms",	NULL },
-	{ "ntime",	"stop",		NULL },
-	{ "nushort",	"stopms",	NULL },
+	{ "ip",		"nexthop",	NULL },
+	{ "time",	"start",	NULL },
+	{ "ushort",	"startms",	NULL },
+	{ "time",	"stop",		NULL },
+	{ "ushort",	"stopms",	NULL },
 	{ "ubyte",	"ipprotocol",	NULL },
-	{ "nushort",	"prompt",	NULL },
-	{ "bytes",	"data",		get_sv4_data },
+	{ "ushort",	"prompt",	NULL },
+	{ "bytes",	"payload",	get_sv4_data },
         { END,		NULL,		NULL }
 };
 
