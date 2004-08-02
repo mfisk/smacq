0d7acd78edd800fe601eb3df51bb828b0f02ade2
Mike Fisk <mfisk@lanl.gov>
2004-08-02 11:35:40 -0600

    new files

---
+++ include/DynamicArray.h
@@ -0,0 +1,19 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+
+#include <vector>
+
+/// This is a wrapper around vector which grows the array
+/// as necessary to satisfy [] operations.
+template <class T>
+class DynamicArray : public std::vector<T> {
+   public:
+        T& operator[](const unsigned int x) {
+                if (x >= size())
+                        resize(x+1);
+                return (std::vector<T>::operator [](x));
+        }
+};
+
+#endif
+
---
+++ libsmacq/smacq_result.cpp
@@ -0,0 +1,9 @@
+#include <smacq.h>
+
+smacq_result SMACQ_NONE(0);
+smacq_result SMACQ_FREE(1);
+smacq_result SMACQ_PASS(2);
+smacq_result SMACQ_ERROR(4);
+smacq_result SMACQ_END(8);
+smacq_result SMACQ_CANPRODUCE(256);
+smacq_result SMACQ_PRODUCE(512);
