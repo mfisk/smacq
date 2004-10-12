471411686a71499d1459fcf0d17936df0753c9b8
Mike Fisk <mfisk@lanl.gov>
2004-10-12 10:14:03 -0600

    bugfix: some objects created with new were freed rather than destroyed; thorough replacement of malloc/free with new.  dts_field to DtsField change.

--- include/dts-filter.h
+++ include/dts-filter.h
@@ -14,24 +14,29 @@ enum dts_arith_operand_type { ADD, SUB, MULT, DIVIDE };
 
 struct dts_operand;
 
-struct dts_arith_operand {
+class dts_arith_operand {
+ public:
   enum dts_arith_operand_type type;
   struct dts_operand * op1;
   struct dts_operand * op2;
 };
 
-struct dts_literal_operand{
+class dts_literal_operand {
+ public:
+  dts_literal_operand() : str(NULL) {}
+
   char * str;
-  dts_field field;
+  DtsField field;
 };
 
-struct dts_operand {
+class dts_operand {
+ public:
   enum dts_operand_type type;
   DtsObject valueo;
 
-  union { 
-    struct dts_arith_operand arith;
-    struct dts_literal_operand literal;
+  struct { 
+    dts_arith_operand arith;
+    dts_literal_operand literal;
   } origin;
 
 };
@@ -43,14 +48,17 @@ struct dts_comp_func {
   char ** argv;
 };
 
-typedef struct _dts_comparison {
+class dts_comparison {
+ public:
+  dts_comparison() : op1(NULL), op2(NULL), next(NULL), group(NULL) {}
+
   dts_compare_operation op;
 
-  struct dts_operand * op1, * op2;
+  dts_operand * op1, * op2;
 
-  struct _dts_comparison * next;
-  struct _dts_comparison * group;
-  struct dts_comp_func func;
-} dts_comparison;
+  dts_comparison * next;
+  dts_comparison * group;
+  dts_comp_func func;
+};
 
 #endif
