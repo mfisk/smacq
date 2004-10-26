c7ef4615b6bef222e0b94a96ff4d1220e141a38f
Mike Fisk <mfisk@lanl.gov>
2004-10-26 13:57:18 -0600

    *** empty log message ***

---
+++ modules/streamalias.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <smacq.h>
+#include <FieldVec.h>
+
+SMACQ_MODULE(streamalias,
+  PROTO_CTOR(streamalias);
+  PROTO_CONSUME();
+
+  DtsField newfield;
+  dts_typeid emptytype;
+); 
+ 
+smacq_result streamaliasModule::consume(DtsObject datum, int & outchan) {
+  DtsObject newd = dts->newObject(emptytype);
+  newd->attach_field(newfield, datum);
+  enqueue(newd, 0);
+
+  return SMACQ_FREE;
+}
+
+streamaliasModule::streamaliasModule(struct SmacqModule::smacq_init * context) : SmacqModule(context) {
+  int argc = context->argc-1;
+  char ** argv = context->argv+1;;
+
+  assert(argc == 1);
+  newfield = dts->requirefield(argv[0]);
+  emptytype = dts->requiretype("empty");
+}
+
