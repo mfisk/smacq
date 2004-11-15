0de5589959b71503143b3bfb202ea3bb9860fe41
Mike Fisk <mfisk@lanl.gov>
2004-11-14 17:50:48 -0700

    not gready with scheduler now; added UNTILs; WHERE clauses now checked with decide()

--- modules/join.cpp
+++ modules/join.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <vector>
+#include <map>
 
 #include <smacq.h>
 #include <FieldVec.h>
 #include <SmacqGraph.h>
 #include <SmacqScheduler.h>
-#include <vector>
-#include <map>
+
+struct alias {
+  DtsField field;
+  DtsField newfield;
+  SmacqScheduler * until;
+  std::vector<DtsObject> objects;
+};
 
 SMACQ_MODULE(join,
   PROTO_CTOR(join);
   PROTO_CONSUME();
 
-  SmacqScheduler * sched;
-  SmacqGraph * where;
-
-  std::vector<DtsField> Aliases;
-  std::vector<std::vector<DtsObject> > Objects;
-  std::vector<SmacqGraph*> Expirations;
-  std::map<std::vector<bool>, SmacqGraph* > Filters;
-
  private:
+
+  SmacqScheduler * sched;
+  SmacqScheduler * where;
+  dts_typeid emptytype;
+
+  std::vector<struct alias> Aliases;
+
   void for_all_but(unsigned int is_alias, DtsObject o, unsigned int alias);
 ); 
 
@@ -32,76 +38,105 @@ SMACQ_MODULE(join,
 // XXX:  We need only evaluate expressions that we haven't already evaluated for
 // the new object.
 //
-// XXX:  It's inefficient to make a zillion sched->input() calls before letting
-// the scheduler run any of them.
-//
 void joinModule::for_all_but(unsigned int is_alias, DtsObject o, unsigned int alias) {
   if (alias == Aliases.size()) {
-	// Okay, test this join.
-	// Dup the object, because we're about to assign new alias values.
-	sched->input(where, o->dup());	
-	return;
+		// Okay, test this join.
+		if (!where || (SMACQ_PASS == where->decide(o))) {
+			// Dup the object, because we're about to assign new alias values.
+	    DtsObject cpy = o->dup();
+		  //fprintf(stderr, "passing joined obj %p\n", cpy.get());
+			enqueue(cpy, 0);	
+	  }
+		return;
   }
 
   if (is_alias != alias) {
-	for (unsigned int j = 0; j < Objects[alias].size(); j++) {
-		o->attach_field(Aliases[alias], Objects[alias][j]);
-		for_all_but(is_alias, o, alias+1);
-	}
+		for (unsigned int j = 0; j < Aliases[alias].objects.size(); j++) {
+			o->attach_field(Aliases[alias].field, Aliases[alias].objects[j]);
+			//fprintf(stderr, "attached %p to %p\n", Aliases[alias].objects[j].get(), o.get());
+			for_all_but(is_alias, o, alias+1);
+		}
   } else {
-	for_all_but(is_alias, o, alias+1);
+		for_all_but(is_alias, o, alias+1);
   }
 }
   
 smacq_result joinModule::consume(DtsObject datum, int & outchan) {
   // Find which alias we just got a new input for
-  for (unsigned int i=0; i<Aliases.size(); i++) {
-	DtsObject o = datum->getfield(Aliases[i]);
-	if (!o) continue;
+  unsigned int num_aliases = Aliases.size();
+  for (unsigned int i=0; i < num_aliases; i++) {
+		alias & a = Aliases[i];
 
-  	// XXX. Remove any stored objects that pass Expiration filters.
-  	;
+		DtsObject o = datum->getfield(a.field);
+		if (!o) continue;
 
-  	// XXX. Then need to remove objects that used to pass relational 
-  	// filters, but don't now that another object has expired.
-  	;
+		// Remove any stored objects that pass UNTIL graph.
+		// XXX.  This only tests against new instances of same alias.
+		for (unsigned int j=0; j < a.objects.size(); j++) {
+			// Build test join
+			DtsObject t = dts->newObject(emptytype);
+			t->attach_field(a.newfield, datum);
+			t->attach_field(a.field, a.objects[j]);
+
+			if (a.until && (SMACQ_PASS == a.until->decide(t))) {
+				// Remove j'th element
+				// (by swapping and shrinking)
+				a.objects[j] = a.objects.back();
+				a.objects.pop_back();
+			}
+		}
+
+		// XXX. Then need to remove objects that used to pass relational 
+		// filters, but don't now that another object has expired.
+		;
 
   	// Test pending joins with this object
-       	for_all_but(i, datum, 0);
+		for_all_but(i, datum, 0);
 		
-	// Save this object.
-	// XXX.  Should save only if it passes the appropriate non-relational filters.
-	Objects[i].push_back(o);
+		// Save this object.
+		// XXX.  Should save only if it passes the appropriate non-relational filters.
+		a.objects.push_back(o);
 
-	break;
+		//fprintf(stderr, "got alias #%d of %d\n", i, Aliases.size());
+
+		break;
   }
 
-  return SMACQ_FREE;
+	//fprintf(stderr, "join canproduce? %d.\n", canproduce()==SMACQ_PRODUCE);
+  return (SMACQ_FREE|canproduce());
 }
 
-joinModule::joinModule(struct SmacqModule::smacq_init * context) 
+joinModule::joinModule(SmacqModule::smacq_init * context) 
   : SmacqModule(context), 
     sched(context->scheduler)
 {
+  emptytype = dts->requiretype("empty");
+
   // Size per-alias data-structures
-  Objects.resize(context->argc-2);
-  Aliases.resize(context->argc-2);
+  Aliases.resize((context->argc-2)/2);
 
   // Copy list of alias names to list of DtsFields
-  for (int i = 1; i < context->argc-1; i++) {
-	Aliases[i-1] = dts->requirefield(context->argv[i]);
+  for (int i = 1; i < context->argc-1; i+=2) {
+		alias & a = Aliases[(i-1)/2];
+		a.field = dts->requirefield(context->argv[i]);
+		a.newfield = dts->requirefield("new");
+		SmacqGraph * until_graph = (SmacqGraph*)strtol(context->argv[i+1], NULL, 0);
+		if (until_graph) { // can be NULL
+			a.until = new SmacqScheduler(dts, until_graph, false);
+			until_graph->init(dts, a.until);	
+		} else {
+			a.until = NULL;
+		}
   }
 
-  where = NULL;
-  where = (SmacqGraph*)strtol(context->argv[context->argc-1], NULL, 0);
-  if (!where) {
-	// where is null, so just send everything to our children.
-	where = context->self->children_as_heads();
+  SmacqGraph * where_graph = (SmacqGraph*)strtol(context->argv[context->argc-1], NULL, 0);
+  if (!where_graph) {
+		where = NULL;
   } else {
-	// run where tests first, then pass results to our children
-  	where->join(context->self->children_as_heads());
+		where = new SmacqScheduler(dts, where_graph, false);
+		where_graph->init(dts, where);
   }
-  where->init(dts, context->scheduler);
+  where_graph->print(stderr, 4);
 
 }
 
