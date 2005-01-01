f5d65248e51421a7748af29b1654d8e9a6aca96d
Mike Fisk <mfisk@lanl.gov>
2004-12-31 18:23:07 -0700

    "*" removed; specifying no fields now prints all fields

--- modules/print.cpp
+++ modules/print.cpp
@@ -8,41 +8,42 @@ SMACQ_MODULE(print,
 	PROTO_CTOR(print);
 	PROTO_CONSUME();
 
-	     char ** argv;
-	     int argc;
-	     int verbose;
-	     int tagged;
-	     int flush;
-	     std::vector<DtsField> fields;
-	     DtsField string_transform;
-	     char * delimiter;
-	     
-	     int print_field(DtsObject field, char * fname, int printed, int column);
-	     );
+   	char ** argv;
+   	int argc;
+	bool verbose;
+	bool tagged;
+	bool flush;
+	bool internals;
+   	std::vector<DtsField> fields;
+	DtsField string_transform;
+	char * delimiter;
+	
+	int print_field(DtsObject field, char * fname, int printed, int column);
+);
 
 static struct smacq_options options[] = {
   {"x", {boolean_t:0}, "XML-markup mode", SMACQ_OPT_TYPE_BOOLEAN},
   {"v", {boolean_t:0}, "Verbose mode: print field names", SMACQ_OPT_TYPE_BOOLEAN},
+  {"i", {boolean_t:0}, "Print internal debugging info", SMACQ_OPT_TYPE_BOOLEAN},
   {"d", {string_t:"\t"}, "Delimiter", SMACQ_OPT_TYPE_STRING},
   {"B", {boolean_t:0}, "Disable buffering: flush output after each line", SMACQ_OPT_TYPE_BOOLEAN},
   END_SMACQ_OPTIONS
 };
 
 int printModule::print_field(DtsObject field, char * fname, int printed, int column) {
-        if (printed) {
-      	   printf(delimiter);
-    	} else if (field) {
-	   int j;
-    	   for (j=0; j<column; j++) 
-    		printf(delimiter);
-        }
+	if (printed) {
+		printf(delimiter);
+   	} else if (field) {
+		for (int j=0; j<column; j++) 
+			printf(delimiter);
+   	}
 
 	if (verbose) printed = 1;
 	if (!field) return printed;
 
 	if (tagged) {
 		printf("<%s>%s</%s>", fname, (char *)field->getdata(),fname);
-	} else if (verbose) {
+	} else if (internals || verbose) {
 		printf("%.20s = %s", fname, (char *)field->getdata());
 	} else {
 		printf("%s", (char*)field->getdata());
@@ -59,9 +60,12 @@ smacq_result printModule::consume(DtsObject datum, int & outchan) {
   DtsObject field;
   assert(datum);
 
-  for (i = 0; i < argc; i++) {
-    if (!fields[i].size()) {
+  if (!fields.size()) {
       /* Print all fields */
+
+	  if (internals) {
+		printf("Object %p:%s", datum.get(), delimiter);
+  	  }
 
       DtsField f;
       f.resize(1);
@@ -71,14 +75,18 @@ smacq_result printModule::consume(DtsObject datum, int & outchan) {
       int num;
 
       for (num=0, j=v.begin(); j != v.end(); ++j, ++num) {
-				if (*j) {
+			if (*j) {
+				DtsObject str = (*j)->getfield(string_transform);
+				if (str) {
 					f[0] = num;
-					printed = print_field(*j, dts->field_getname(f), printed, column);
+					printed = print_field(str, dts->field_getname(f), printed, column);
 					column++;
 				}
+			}
       }
 
-    } else {
+  } else {
+	for (i = 0; i < argc; i++) {
 			field = datum->getfield(fields[i]);
 			printed = print_field(field, argv[i], printed, column);
 
@@ -86,11 +94,11 @@ smacq_result printModule::consume(DtsObject datum, int & outchan) {
 				if (tagged) {
 					printf("no field %s.string", argv[i]);
 				} else {
-       		fprintf(stderr, "Warning: print: no field %s.string\n", argv[i]);
+       				fprintf(stderr, "Warning: print: no field %s.string\n", argv[i]);
 				}
 			}
-		}
-    column++;
+    		column++;
+	}
   }
   if (printed) {
  		printf("\n");
@@ -102,7 +110,7 @@ smacq_result printModule::consume(DtsObject datum, int & outchan) {
 }
 
 printModule::printModule(struct smacq_init * context) : SmacqModule(context) {
-  smacq_opt tagged_opt, verbose_opt, flush_opt, delimiter_opt;
+  smacq_opt tagged_opt, verbose_opt, flush_opt, delimiter_opt, internals_opt;
   int i;
 
   {
@@ -111,14 +119,16 @@ printModule::printModule(struct smacq_init * context) : SmacqModule(context) {
       {"v", &verbose_opt},
       {"d", &delimiter_opt},
       {"B", &flush_opt},
+      {"i", &internals_opt},
       {NULL, NULL}
     };
     smacq_getoptsbyname(context->argc-1, context->argv+1,
 				 &argc, &argv,
 				 options, optvals);
-    assert(argv);
+    //assert(argv);
   }
 
+  internals = internals_opt.boolean_t;
   flush = flush_opt.boolean_t;
   delimiter = delimiter_opt.string_t;
   verbose = verbose_opt.boolean_t;
@@ -131,9 +141,7 @@ printModule::printModule(struct smacq_init * context) : SmacqModule(context) {
   //fprintf(stderr, "creating2 print module\n");
 
   for (i = 0; i < argc; i++) {
-	  if (strcmp(argv[i], "*")) {
-	  	fields[i] = dts->requirefield(dts_fieldname_append(argv[i],"string")); 
-	  }
+	  fields[i] = dts->requirefield(dts_fieldname_append(argv[i],"string")); 
   }
 }
 
