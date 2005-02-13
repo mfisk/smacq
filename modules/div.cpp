99f4b2545d8e3222328e8c7b0e08040a4d09dbb8
Mike Fisk <mfisk@lanl.gov>
2005-02-13 11:27:52 -0700

    div is not clock; should not refresh

--- modules/div.cpp
+++ modules/div.cpp
@@ -8,65 +8,45 @@
 #include <assert.h>
 
 #define SMACQ_MODULE_IS_ANNOTATION 1
+#define SMACQ_MODULE_IS_STATELESS 1
 #include <SmacqModule.h>
-#include <FieldVec.h>
 #include <FieldVec.h>
 
 static struct smacq_options options[] = {
-  {"t", {double_t:1}, "Div tick length", SMACQ_OPT_TYPE_DOUBLE},
+  {"d", {double_t:1}, "Divisor", SMACQ_OPT_TYPE_DOUBLE},
   END_SMACQ_OPTIONS
 };
 
 
 SMACQ_MODULE(div,
   PROTO_CTOR(div);
-  PROTO_DTOR(div);
   PROTO_CONSUME();
 
   DtsField timefield;
   DtsField divfield;
-  int refreshtype;
   int ticktype;
-  double period;
+  double divisor;
 
-  long int current_tick;
-  DtsObject current_ticko;
+  int current_tick;
+  DtsObject ticko;
 );
 
 smacq_result divModule::consume(DtsObject datum, int & outchan) {
   double ts;
   long int tick;
 
-  {
-      	DtsObject time;
-	time = datum->getfield(timefield);
-  	if (!time) return SMACQ_PASS;
+  DtsObject time = datum->getfield(timefield);
+  if (!time) return SMACQ_PASS;
 
-  	ts = dts_data_as(time, double);
-  	
-  }
-  tick = (int)(ts / period);
-  /* fprintf(stderr, "got time tick %ld (period %f)\n", tick, period); */
+  ts = dts_data_as(time, double);
+  tick = (int)(ts / divisor);
 
-  /* only change div forward */
   if ( tick != current_tick) {
-  	DtsObject ticko; 
-  	/* fprintf(stderr, "div advanced to %ld (period %f)\n", tick, period); */
-
-	DtsObject refresh = dts->newObject(refreshtype);
-	refresh->attach_field(divfield, current_ticko);
-  	//fprintf(stderr, "div is queueing %p (a refresh of type %d)\n", refresh, refreshtype);
-	enqueue(refresh, outchan);
-
-  	ticko = dts->construct(ticktype, &tick); 
+  	DtsObject ticko = dts->construct(ticktype, &tick); 
   	assert(ticko);
-
 	current_tick = tick;
-	current_ticko = ticko;
   }
-
-  datum->attach_field(divfield, current_ticko);
-  
+  datum->attach_field(divfield, ticko);
 
   return SMACQ_PASS;
 }
@@ -76,10 +56,10 @@ divModule::divModule(struct SmacqModule::smacq_init * context) : SmacqModule(con
   char ** argv;
 
   {
-    smacq_opt period_opt;
+    smacq_opt div_opt;
 
     struct smacq_optval optvals[] = {
-      {"t", &period_opt},
+      {"d", &div_opt},
       {NULL, NULL}
     };
 
@@ -87,19 +67,12 @@ divModule::divModule(struct SmacqModule::smacq_init * context) : SmacqModule(con
 		       &argc, &argv,
 		       options, optvals);
 
-    period = period_opt.double_t;
+    divisor = div_opt.double_t;
   }
 
   assert(argc);
   timefield = dts->requirefield(dts_fieldname_append(argv[0],"double"));
   divfield = dts->requirefield("div");
-  refreshtype = dts->requiretype("refresh");
   ticktype = dts->requiretype("int");
-}
-
-divModule::~divModule() {
-  DtsObject datum = dts->construct(refreshtype, NULL);
-  datum->attach_field(divfield, current_ticko);
-  enqueue(datum);
 }
 
