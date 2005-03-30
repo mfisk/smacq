e0a3bee661d114291f6d316e020c97274292f0ce
Mike Fisk <mfisk@lanl.gov>
2005-03-30 13:08:51 -0700

    more standard flags

--- modules/const.cpp
+++ modules/const.cpp
@@ -6,6 +6,12 @@
 
 #include <SmacqModule.h>
 
+static struct smacq_options options[] = {
+	{"t", {string_t:"string"}, "Type", SMACQ_OPT_TYPE_STRING},
+	{"f", {string_t:"const"}, "Name of annotation field", SMACQ_OPT_TYPE_STRING},
+	END_SMACQ_OPTIONS
+};
+
 SMACQ_MODULE(const,
   PROTO_CTOR(const);
   PROTO_CONSUME();
@@ -20,17 +26,22 @@ smacq_result constModule::consume(DtsObject datum, int & outchan) {
 }
 
 constModule::constModule(struct SmacqModule::smacq_init * context) : SmacqModule(context) {
-  int argc = context->argc-1;
-  char ** argv = context->argv+1;
+  int argc;
+  char ** argv;
+  smacq_opt type_opt, field_opt;
 
-  assert(argc<=2);
-  assert(argc>=1);
+  struct smacq_optval optvals[] = {
+	  {"t", &type_opt},
+	  {"f", &field_opt},
+	  {NULL, NULL}
+  };
+  smacq_getoptsbyname(context->argc-1, context->argv+1,
+                      &argc, &argv,
+                      options, optvals);
 
-  data = dts->construct_fromstring(dts->requiretype("string"), strdup(argv[0]));
-  if (argc == 2) {
-	field = dts->requirefield(argv[1]);
-  } else {
-	field = dts->requirefield("const");
-  }
+  assert(argc==1);
+
+  data = dts->construct_fromstring(dts->requiretype(type_opt.string_t), strdup(argv[0]));
+  field = dts->requirefield(field_opt.string_t);
 }
 
