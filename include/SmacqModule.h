af39dffc83b6eeb574017ffafba07d513a2af20e
Mike Fisk <mfisk@lanl.gov>
2005-06-20 09:40:33 -0600

    added consume_all()

--- include/SmacqModule.h
+++ include/SmacqModule.h
@@ -232,6 +232,15 @@ struct smacq_functions {
 
 inline void SmacqModule::enqueue(DtsObject & datum, int outchan) {
   scheduler->queue_children(self, datum, outchan);
+
+  /* 
+   * Since we added work, surrender to the scheduler.
+   * This really should be a co-routine sort of thing, but instead we take the simpler
+   * approach of using a special entry point to the scheduler that will drain
+   * the consume queue, but won't handle work that requires returning data
+   * to the caller of the scheduler that called us.  
+   */
+  scheduler->consume_all();
 }
 
 #endif	
