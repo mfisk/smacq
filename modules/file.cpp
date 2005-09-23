62e39c101b5901fd322802453e9de10a2dcb8c5b
Mike Fisk <mfisk@lanl.gov>
2005-09-23 13:43:09 -0600

    added -F

--- modules/file.cpp
+++ modules/file.cpp
@@ -6,6 +6,7 @@
 
 static struct smacq_options options[] = {
   {"l", {boolean_t:false}, "Read list of files from stdin", SMACQ_OPT_TYPE_BOOLEAN},
+  {"F", {boolean_t:false}, "Follow files", SMACQ_OPT_TYPE_BOOLEAN},
   END_SMACQ_OPTIONS
 };
 
@@ -17,12 +18,14 @@ SMACQ_MODULE(file,
   char ** argv;
 
   bool use_stdin;
+  DtsObject followObject;
+  DtsField followField; 
   dts_typeid string_type;
 );
 
 
 fileModule::fileModule(struct SmacqModule::smacq_init * context) : SmacqModule(context) {
-  smacq_opt list;
+  smacq_opt list, follow;
 
   //fprintf(stderr, "Loading pcapfile (%d,%d)\n", context->isfirst, context->islast);
   dts->requiretype("packet");
@@ -30,6 +33,7 @@ fileModule::fileModule(struct SmacqModule::smacq_init * context) : SmacqModule(c
   {
     struct smacq_optval optvals[] = {
       { "l", &list}, 
+      { "F", &follow}, 
       {NULL, NULL}
     };
     smacq_getoptsbyname(context->argc-1, context->argv+1,
@@ -38,6 +42,10 @@ fileModule::fileModule(struct SmacqModule::smacq_init * context) : SmacqModule(c
     use_stdin = list.boolean_t;
   }
   string_type = dts->requiretype("string");
+  if (follow.boolean_t) {
+ 	followObject = dts->newObject(dts->requiretype("empty"));
+	followField = dts->requirefield("doFollow");
+  } 
 
   if (!argc && !use_stdin) {
 	/* Read directly from stdin */
@@ -54,12 +62,14 @@ smacq_result fileModule::produce(DtsObject & o, int & outchan) {
 		if (filename[0] != '\0') {
 			o = dts->construct_fromstring(string_type, filename);
 			assert(o);
+			if (followObject) { o->attach_field(followField, followObject); }
 			return SMACQ_PASS|SMACQ_PRODUCE;
 		} else {
 			return SMACQ_END;
 		}
 	} else if (argc) {
 		o = dts->construct_fromstring(string_type, argv[0]);
+		if (followObject) { o->attach_field(followField, followObject); }
 		argv++;
 		argc--;
 		return SMACQ_PASS|SMACQ_PRODUCE;
