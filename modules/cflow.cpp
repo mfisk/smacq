47ef606e01dd75b07e717ed8dd3c7f345263fae2
Mike Fisk <mfisk@lanl.gov>
2005-09-28 13:52:33 -0600

    fixed cflowModule to no be a hog and fill outputq

--- modules/cflow.cpp
+++ modules/cflow.cpp
@@ -4,24 +4,33 @@
 
 SMACQ_MODULE(cflow,
   PROTO_CTOR(cflow);
+  PROTO_DTOR(cflow);
   PROTO_CONSUME();
+  PROTO_PRODUCE();
 
   int cflow_type;
+  StrucioStream * fh;
 );
 
-smacq_result cflowModule::consume(DtsObject datum, int & outchan) {
-  StrucioStream * fh = StrucioStream::MagicOpen(datum);
-  DtsObject o;
-  if (fh) {
-	while ((o = fh->construct(dts, cflow_type))) {
-		enqueue(o);
-	}
+smacq_result cflowModule::produce(DtsObject & datum, int & outchan) {
+  if (fh && (datum = fh->construct(dts, cflow_type))) {
+		return SMACQ_PASS|SMACQ_PRODUCE;
+	} else {
+		return SMACQ_FREE;
   }
-  return SMACQ_FREE;
 }
 
-cflowModule::cflowModule(struct SmacqModule::smacq_init * context) : SmacqModule(context) {
+smacq_result cflowModule::consume(DtsObject datum, int & outchan) {
+  fh = StrucioStream::MagicOpen(datum);
+  return SMACQ_FREE|SMACQ_PRODUCE;
+}
+
+cflowModule::cflowModule(struct SmacqModule::smacq_init * context) : SmacqModule(context), fh(NULL) {
  SmacqFileModule(context);
 
  cflow_type = dts->requiretype("cflow");
+}
+
+cflowModule::~cflowModule() {
+ if (fh) delete fh;
 }
