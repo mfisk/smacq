c424f6673ede39a1bc0b364eba201b1e3831986a
Mike Fisk <mfisk@lanl.gov>
2005-11-05 17:10:49 -0700

    refactor

--- bin/psmacqq.cpp
+++ bin/psmacqq.cpp
@@ -1,20 +1,14 @@
 #include <smacq_am.h>
 
 int main(int argc, char ** argv) {
-  int retval;
-  retval = gasnet_init(&argc, &argv);
-  assert (retval == GASNET_OK);
-  retval = gasnet_attach(handlers, numHandlers, 0, 0);
-  assert (retval == GASNET_OK);
+  init_am(&argc, &argv);
 
   if (gasnet_mynode() == 0) {
 	smacqq(argc, argv);
   	gasnet_exit(0);
   } else {
-	bool done = false;
-	for (;;) {
-		GASNET_BLOCKUNTIL(done);
-	}
+	smacq_am_slave_loop();
+	return 0;
   }
 }
 
