640917fef7c8e212cab1ada40442159d0e53305c
Mike Fisk <mfisk@lanl.gov>
2005-11-05 17:04:08 -0700

    interim

--- modules/distribute.cpp
+++ modules/distribute.cpp
@@ -7,6 +7,8 @@
 #include <SmacqGraph.h>
 #include <SmacqScheduler.h>
 
+#include <smacq_am.h>
+
 static struct smacq_options options[] = {
   {"seed", {boolean_t:0}, "Seed remote graph to produce", SMACQ_OPT_TYPE_BOOLEAN},
   END_SMACQ_OPTIONS
@@ -17,19 +19,25 @@ SMACQ_MODULE(distribute,
   PROTO_DTOR(distribute);
   PROTO_CONSUME();
   PROTO_PRODUCE();
+
+  int nodes;
+  int node_iterator ni;
 ); 
 
 smacq_result groupbyModule::consume(DtsObject datum, int & outchan) {
-  
+  // Send object to all other nodes
+  Gasnet.RequestMedium(ni, AM_DTSOBJECT, datum->getdata(), datum->getsize(), datum->gettype());
 
+  ni = ((ni + 1) % (nodes-1)) + 1; // Iterate over [1..nodes)
   return SMACQ_FREE;
 }
 
 groupbyModule::~groupbyModule() {
+  Gasnet.RequestShort(i, AM_ENDPOINT);
 }
 
 groupbyModule::groupbyModule(struct SmacqModule::smacq_init * context) 
-  : SmacqModule(context), self(context->self)
+  : SmacqModule(context), self(context->self), ni(1);
 {
   int argc;
   char ** argv;
@@ -43,19 +51,11 @@ groupbyModule::groupbyModule(struct SmacqModule::smacq_init * context)
 			       &argc, &argv,
 			       options, optvals);
 
-  char ** exec_argv = (char**)malloc(sizeof(char*)*4);
-  exec_argv[0] = "smacqq";
-  exec_argv[1] = "slave";
-  exec_argv[2] = argv[0];
-
-  if (fork) {
-	// parent
-	/// XXX. Handle AM callbacks
-  } else {
-	// child
-	execvp(exec_argv[0], exec_argv);
-	exit(-1); // Shouldn't get here
+  // Distribute query to all other nodes
+  for (unsigned int i = 1; i < nodes; ++i) {
+  	Gasnet.RequestMedium(i, AM_QUERY, argv[0], strlen(argv[0])+1, seed.bool_t, context->self);
   }
-}
 
+  nodes = gasnet_nodes();
+}
 
