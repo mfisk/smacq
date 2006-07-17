6a9bb6d7749d0004edf90907cf5856e4efcd0f76
Mike Fisk <mfisk@lanl.gov>
2006-07-17 17:32:44 -0600

    new take module decapsulates an object

---
+++ modules/take.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <SmacqModule.h>
+
+SMACQ_MODULE(take,
+  PROTO_CTOR(take);
+  PROTO_CONSUME();
+
+  DtsField field;
+); 
+ 
+smacq_result takeModule::consume(DtsObject datum, int & outchan) {
+  DtsObject inside = datum->getfield(field);
+  enqueue(inside);
+  return SMACQ_FREE;
+}
+
+takeModule::takeModule(struct SmacqModule::smacq_init * context) : SmacqModule(context) {
+  int argc = context->argc-1;
+  char ** argv = context->argv+1;;
+
+  assert(argc==1);
+  field = dts->requirefield(argv[0]);
+
+}
+
