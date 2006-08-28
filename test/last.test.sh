5e5037cf363170fc76e1bdaf8a051ce819409c9e
Mike Fisk <mfisk@lanl.gov>
2006-08-27 19:25:49 -0600

    remove -m option

--- test/last.test.sh
+++ test/last.test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-$SMACQQ -g -m <<EOF | sort -n
+$SMACQQ -g -f - <<EOF | sort -n
 print ts, srcip, dstip from (last srcip, dstip from pcapfile(0325@1112-snort.pcap))
 EOF
--- test/sqlite.test.sh
+++ test/sqlite.test.sh
@@ -7,11 +7,11 @@ rm -f $DB
 #create table foo(ts varchar(80), srcip varchar(32), dstip varchar(32));
 #EOM
 
-$SMACQQ -m -g <<EOF 
+$SMACQQ -f - -g <<EOF 
 sqlinsert -t foo -d "URI=$DB" ts srcip dstip from (uniq srcip, dstip from pcapfile(0325@1112-snort.pcap))
 EOF
 
-$SMACQQ -m -g <<EOF 
+$SMACQQ -f - -g <<EOF 
 print -v ts srcip dstip from sqlquery(-d "URI=$DB" foo)
 EOF
 
