0fff98c5cccdeae8c7fd49bbb943056493308caf
Mike Fisk <mfisk@lanl.gov>
2006-10-08 17:35:21 -0600

    changed test case some

--- test/pysmacq.test.sh
+++ test/pysmacq.test.sh
@@ -11,23 +11,14 @@ import smacq
 print "Imported successfully"
 
 pcapfile = '0325@1112-snort.pcap'
-query = 'print from pcapfile(%s)' % (pcapfile)
 
-q1 = 'print srcip dstip srcport dstport' 
-q2 = 'pcapfile(%s)' % (pcapfile)
+q = smacq.SmacqQuery('pcapfile(%s)' % (pcapfile))
+q >>= 'where srcport > 137'
 
-g2 = smacq.SmacqQuery(q2)
-g1 = smacq.SmacqQuery(q1)
+print q
 
-print g1
-print g2
+for d in q:
+	print d['srcip'], d['dstip'], d['srcport'], d['dstport'], d.has_key('foo')
 
-g2 += g1
-
-print g2
-
-g2.run()
-
-g2.busy_loop()
 EOF
 
