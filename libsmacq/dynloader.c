2493d6f599e5a5794933282a40c1e415bdad71cf
Mike Fisk <mfisk@lanl.gov>
2006-10-13 13:13:15 -0600

    use new preload.h to resolve preloaded symbols

--- libsmacq/dynloader.c
+++ libsmacq/dynloader.c
@@ -5,6 +5,26 @@
 #include <assert.h>
 #include <string.h>
 
+struct preload_entry {
+	void * ptr;
+	char * sym;
+};
+#include "preload.h"
+
+static void * smacq_try_dlfindsym_preload(char * format, char * sym) {
+    char trysym[1024];
+    snprintf(trysym, 1023, format, sym);
+    struct preload_entry *p;
+    for (p = preloads; p->sym; p++) {
+    	//fprintf(stderr, "Comparing %s to %s\n", trysym, p->sym);
+	if (!strcmp(p->sym, trysym)) {
+		return p->ptr;
+	}
+    }
+    fprintf(stderr, "Symbol %s not preloaded\n", trysym);
+    return NULL;
+}
+
 static void * smacq_try_dlfindsym(lt_dlhandle gmodule, char * format, char * sym) {
     char trysym[1024];
     void * modtable;
@@ -29,47 +49,53 @@ static void * smacq_try_dlfindsym(lt_dlhandle gmodule, char * format, char * sym
 }
 
 void * smacq_find_module(lt_dlhandle* gmodulep, char * envvar, char * envdefault, char * modformat, char * symformat, char * sym) {
-    void * self;
     void * modtable = NULL;
 
     assert(gmodulep);
     assert(sym);
 
-    lt_dlinit();
-
-    self = lt_dlopen(NULL);
-    if (!self) {
-	fprintf(stderr, "Warning: %s\n", dlerror());
-    } else {
-    	modtable = smacq_try_dlfindsym(self, symformat, sym);
+    // Try preloaded symbols first
+    modtable = smacq_try_dlfindsym_preload(symformat, sym);
+    if (modtable) {
+	fprintf(stderr, "Found %s in preloads\n", sym);
+	return modtable;
     }
 
-    //printf("dlfindsym found %p\n", modtable);
+    lt_dlinit();
 
-    // Try loading from self using the manual loader.
-    if (modtable) {
-	return modtable;
+/*
+    void * self = lt_dlopen(NULL);
+    if (!self) {
+	fprintf(stderr, "Warning: %s\n", lt_dlerror());
     } else {
-        // If not in self, then try opening a module 
-    	char modfile[256];
-    	char * path = getenv(envvar);
-
-    	// Build shared library location
-    	if (!path) path = envdefault;
-
-    	snprintf(modfile, 256, modformat, path, sym);
-        //printf("Before: %p\n", *gmodulep);
-        *gmodulep = lt_dlopen(modfile);
-        if (! *gmodulep ) {
-           //fprintf(stderr, "%s (%s,%s,%s->%s) (Need to set SMACQ_HOME?, Yo)\n", sym, modformat, path, sym, modfile);
-           return NULL;
-        }
-
-    	modtable = smacq_try_dlfindsym(*gmodulep, symformat, sym);
+        modtable = smacq_try_dlfindsym(self, symformat, sym);
 
         if (modtable) {
-            return modtable;
+    	  //printf("dlfindsym found %p\n", modtable);
+	  return modtable;
         }
+    }
+*/
+
+    // If not in self, then try opening a module 
+    char modfile[256];
+    char * path = getenv(envvar);
+
+    // Build shared library location
+    if (!path) path = envdefault;
+
+    snprintf(modfile, 256, modformat, path, sym);
+    //printf("Before: %p\n", *gmodulep);
+    *gmodulep = lt_dlopen(modfile);
+    if (! *gmodulep ) {
+           //fprintf(stderr, "%s (%s,%s,%s->%s) (Need to set SMACQ_HOME?)\n", sym, modformat, path, sym, modfile);
+           return NULL;
+    }
+
+    modtable = smacq_try_dlfindsym(*gmodulep, symformat, sym);
+
+    if (modtable) {
+            return modtable;
     }
     
     fprintf(stderr, "Error: unable to find symbol %s\n", sym);
