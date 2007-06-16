65cebe47de634c87bde0169c26fcc02959e2b5cd
Mike Fisk <mfisk@lanl.gov>
2007-06-16 16:02:22 -0600

    nevermind

--- types/empty.cpp
+++ types/empty.cpp
@@ -1,11 +1,9 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include <dts-module.h>
 #include "dts.h"
 
 struct dts_type_info dts_type_empty_table = {
-	// gcc-4.1.2 says table is unitialized if set to 0
-        size: 1
+        size: 0
 };
 
