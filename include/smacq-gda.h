166fb3c8f954dd0acde8295a468afb072c963540
Mike Fisk <mfisk@lanl.gov>
2007-06-16 18:16:56 -0600

    got 1.3 wrapper working with 1.2 underneath

--- include/smacq-gda.h
+++ include/smacq-gda.h
@@ -7,35 +7,32 @@
 
 static inline void smacq_gda_set_error(GdaConnection * conn, GError ** err) {
     GList * errorlist = (GList*) gda_connection_get_errors(conn);
-    assert(errorlist);
-    errorlist = g_list_first(errorlist);
-    assert(errorlist);
-    GdaError * error = (GdaError*) errorlist->data;
-    assert(error);
-    g_set_error(err, 0, gda_error_get_number(error), gda_error_get_description(error));
-    if (g_list_next(errorlist)) {
+    if (errorlist) {
+      errorlist = g_list_first(errorlist);
+      assert(errorlist);
+      GdaError * error = (GdaError*) errorlist->data;
+      assert(error);
+      g_set_error(err, 0, gda_error_get_number(error), gda_error_get_description(error));
+      if (g_list_next(errorlist)) {
 	fprintf(stderr, "Warning: additional GDA errors not displayed\n");
+      }
+    } else {
+	fprintf(stderr, "Unknown GDA error\n");
     }
 }
 
 static inline GdaConnection* gda_client_open_connection(GdaClient* client , const gchar* dsn, const gchar* username, const gchar* password, GdaConnectionOptions options, GError ** err) {
   GdaConnection * gdares = gda_client_open_connection(client, dsn, username, password, options);
-  if (! gdares) {
-	smacq_gda_set_error(NULL, err);
-  }
   return gdares;
 }
 static inline GdaConnection* gda_client_open_connection_from_string(GdaClient *client, const gchar *provider_id, const gchar *cnc_string, GdaConnectionOptions options, GError** err) {
   GdaConnection * gdares =  gda_client_open_connection_from_string(client, provider_id, cnc_string, options);
-  if (!gdares) {
-	smacq_gda_set_error(NULL, err);
-  }
   return gdares;
 }
 
 static inline gint gda_connection_execute_non_query(GdaConnection* conn, GdaCommand* cmd, GdaParameterList* params, GError ** err) {
   gint gdares = gda_connection_execute_non_query(conn, cmd, params);
-  if (gdares == -1 ) {
+  if (gdares == -1) {
 	smacq_gda_set_error(conn, err);
 	return false;
   } else {
